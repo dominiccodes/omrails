cb40436b5d645d6a48e622f51ec547029e28f41d
dominiccodes <email@domcodes.com>
2017-06-28 22:54:57 +0100

    generated pages controller, added home, about and contact. Added navigation with erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+	#  get 'pages/home'
+	root 'pages#home'
+	
+	# route to	access the about page
+	get 'about' => 'pages#about'
+	
+	# route to	access the contact page
+	get 'contact' => 'pages#contact'
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
