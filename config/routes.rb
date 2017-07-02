787a6a8b33caa0dbcd761dc4df681762fdb39dbd
dominiccodes <email@domcodes.com>
2017-07-02 18:22:04 +0100

    Fixed navigation issues, configured routes for sign up and sign in and added nav links in front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
+	as :user do
+		get "signin" => 'devise/sessions#new'
+		delete "signout" => 'devise/sessions#destroy'
+		get "signup" => 'devise/registrations#new'
+	end
+	
 	#  get 'pages/home'
 	root 'pages#home'
 	
 	# route to	access the about page
 	get 'about' => 'pages#about'
-	
-	# route to	access the contact page
-	get 'contact' => 'pages#contact'
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
